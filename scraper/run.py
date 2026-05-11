d25892aff9e48f616bcb62254fb294b58211a1cf
Ian O'Gorman <ogorman89@gmail.com>
2026-05-11 15:27:08 -0700

    add instance families, cpu bounds, merge script

--- scraper/run.py
+++ scraper/run.py
@@ -7,6 +7,8 @@ from typing_extensions import TypedDict
 from scraper.ca import KwokNodeList
 from scraper.karpenter import InstanceTypeOptionsList
 
+from scraper.common import fetch_instance_types
+
 
 class Filter(TypedDict):
     Name: str
@@ -32,14 +34,23 @@ def parse_args() -> argparse.Namespace:
     )
 
     root_parser.add_argument(
-        "--instance-types",
-        default=list(),
-        type=lambda s: s.split(",")
+        "--instance-types", default=list(), type=lambda s: s.split(",")
     )
 
     root_parser.add_argument(
-        "--disable-spot",
-        action="store_true"
+        "--instance-families", default=list(), type=lambda s: s.split(",")
+    )
+
+    root_parser.add_argument("--zones", default=list(), type=lambda s: s.split(","))
+
+    root_parser.add_argument("--min-cpu", type=int)
+
+    root_parser.add_argument("--max-cpu", type=int)
+
+    root_parser.add_argument(
+        "--capacity-type",
+        default=["on-demand", "spot"],
+        type=lambda s: s.split(","),
     )
 
     root_parser.add_argument(
@@ -56,7 +67,8 @@ def parse_args() -> argparse.Namespace:
     )
 
     root_parser.add_argument(
-        "--output", "-o",
+        "--output",
+        "-o",
     )
 
     return root_parser.parse_args()
@@ -65,6 +77,16 @@ def parse_args() -> argparse.Namespace:
 def run() -> None:
     args = parse_args()
     ec2 = boto3.client("ec2", region_name=args.aws_region)
+
+    if args.instance_families and args.instance_types:
+        raise ValueError(
+            "Instance_families and instance_types are mutually exclusive options"
+        )
+
+    if args.instance_families and not (args.min_cpu and args.max_cpu):
+        raise ValueError(
+            "If instance families are provided a valid min_cpu and max_cpu must be provided"
+        )
 
     builder: t.Union[InstanceTypeOptionsList, KwokNodeList]
     if args.target == "karpenter":
@@ -76,14 +98,25 @@ def run() -> None:
         if args.output is None:
             args.output = "kwok_provider_config.yaml"
 
-    print(f"Fetching AWS zones for {args.aws_region}")
-    zones = [az["ZoneName"] for az in ec2.describe_availability_zones()["AvailabilityZones"]]
+    zones = args.zones
+    if not args.zones:
+        print(f"Fetching AWS zones for {args.aws_region}")
+        zones = [
+            az["ZoneName"]
+            for az in ec2.describe_availability_zones()["AvailabilityZones"]
+        ]
+
+    instance_types = args.instance_types
+    if args.instance_families:
+        instance_types = fetch_instance_types(
+            ec2, args.instance_families, args.min_cpu, args.max_cpu
+        )
 
     print("Fetching EC2 Instance Types")
     paginator = ec2.get_paginator("describe_instance_types")
-    for page in paginator.paginate(InstanceTypes=args.instance_types, Filters=args.filters):
+    for page in paginator.paginate(InstanceTypes=instance_types, Filters=args.filters):
         for instance_type in page["InstanceTypes"]:
-            builder.add_instance_types(instance_type, zones, args.disable_spot)
+            builder.add_instance_types(instance_type, zones, args.capacity_type)
 
     print(f"Writing data to {args.output}")
     with open(args.output, "w") as f:
