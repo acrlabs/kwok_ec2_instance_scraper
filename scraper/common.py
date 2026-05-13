3f0ae109a799346c504d06c8d766aa69e9ec4be0
Ian O'Gorman <ogorman89@users.noreply.github.com>
2026-05-13 10:39:58 -0700

    feat: filtering by instance families and min-max cpu

--- scraper/common.py
+++ scraper/common.py
@@ -7,7 +7,9 @@ from mypy_boto3_ec2.literals import InstanceTypeType
 from mypy_boto3_ec2.type_defs import InstanceTypeInfoTypeDef
 
 AWS_PRICING_API_ROOT = "https://pricing.us-east-1.amazonaws.com"
-EC2_PRICING_REGION_INDEX_URL = f"{AWS_PRICING_API_ROOT}/offers/v1.0/aws/AmazonEC2/current/region_index.json"
+EC2_PRICING_REGION_INDEX_URL = (
+    f"{AWS_PRICING_API_ROOT}/offers/v1.0/aws/AmazonEC2/current/region_index.json"
+)
 
 
 def convert_arch(arch: str) -> str:
@@ -16,12 +18,43 @@ def convert_arch(arch: str) -> str:
     return arch
 
 
+def fetch_instance_types(
+    ec2: EC2Client, families: list[str], min_cpu: int, max_cpu: int
+) -> list[str]:
+    prefixes = tuple(map(lambda x: x + ".", families))
+    print(prefixes)
+    # Fetch the instance types
+    paginator = ec2.get_paginator("describe_instance_types")
+    instance_types = []
+
+    for page in paginator.paginate():
+        for instance in page["InstanceTypes"]:
+            # Filter by family and CPU constraints
+            name = instance["InstanceType"]
+
+            if not name.startswith(prefixes):
+                continue
+
+            vcpus = instance["VCpuInfo"]["DefaultVCpus"]
+
+            if min_cpu <= vcpus <= max_cpu:
+                instance_types.append(name)
+
+    return instance_types
+
+
 def fetch_ondemand_prices(aws_region: str) -> t.Mapping[str, float]:
     # This is ugly AF, but I blame AWS
 
     print(f"  fetching per-region pricing urls from {EC2_PRICING_REGION_INDEX_URL}")
     region_data = requests.get(EC2_PRICING_REGION_INDEX_URL).json()
-    region_url = AWS_PRICING_API_ROOT + region_data["regions"][aws_region]["currentVersionUrl"]
+    # This is not great but we are pinning us-east-1 for on demand pricing since
+    # the us-east-1 region has the most complete pricing document. We are
+    # most interested in relative pricing but we should see if we get better
+    # pricing from the pricing API at some point.
+    region_url = (
+        AWS_PRICING_API_ROOT + region_data["regions"]["us-east-1"]["currentVersionUrl"]
+    )
 
     print(f"  fetching on-demand pricing data for {aws_region} from {region_url}")
     prices = requests.get(f"{region_url}").json()
@@ -29,20 +62,24 @@ def fetch_ondemand_prices(aws_region: str) -> t.Mapping[str, float]:
     skus_by_instance_type = {
         deets["attributes"]["instanceType"]: sku
         for sku, deets in prices["products"].items()
-        if deets["attributes"].get("usagetype").startswith("BoxUsage") and
-            deets["attributes"]["operatingSystem"] == "Linux"  # noqa
+        if deets["attributes"].get("usagetype").startswith("BoxUsage")
+        and deets["attributes"]["operatingSystem"] == "Linux"  # noqa
     }
 
     prices_by_instance_type = dict()
     for instance_type, sku in skus_by_instance_type.items():
         block = prices["terms"]["OnDemand"][sku]
         price_dimensions = list(block.values())[0]["priceDimensions"]
-        prices_by_instance_type[instance_type] = float(list(price_dimensions.values())[0]["pricePerUnit"]["USD"])
+        prices_by_instance_type[instance_type] = float(
+            list(price_dimensions.values())[0]["pricePerUnit"]["USD"]
+        )
 
     return prices_by_instance_type
 
 
-def fetch_spot_prices(ec2: EC2Client) -> t.Mapping[t.Tuple[InstanceTypeType, str], float]:
+def fetch_spot_prices(
+    ec2: EC2Client,
+) -> t.Mapping[t.Tuple[InstanceTypeType, str], float]:
     print("  fetching spot pricing data")
     paginator = ec2.get_paginator("describe_spot_price_history")
     end = arrow.utcnow()
@@ -55,12 +92,16 @@ def fetch_spot_prices(ec2: EC2Client) -> t.Mapping[t.Tuple[InstanceTypeType, str
         EndTime=end.format("YYYY-MM-DDTHH:mm:ssZ"),
     ):
         for item in page["SpotPriceHistory"]:
-            spot_prices_by_instance_type[(item["InstanceType"], item["AvailabilityZone"])] = float(item["SpotPrice"])
+            spot_prices_by_instance_type[
+                (item["InstanceType"], item["AvailabilityZone"])
+            ] = float(item["SpotPrice"])
 
     return spot_prices_by_instance_type
 
 
-def make_resource_map(instance_type: InstanceTypeInfoTypeDef) -> t.MutableMapping[str, str]:
+def make_resource_map(
+    instance_type: InstanceTypeInfoTypeDef,
+) -> t.MutableMapping[str, str]:
     resource_map = {}
 
     cpu = instance_type.get("VCpuInfo", {}).get("DefaultVCpus")
@@ -88,9 +129,6 @@ def is_valid_usage_class(
     name: str,
     zone: str,
     spot_instances: t.KeysView[t.Tuple[InstanceTypeType, str]],
-    disable_spot: bool,
+    capacity_type: list[str],
 ) -> bool:
-    return (
-        cl == "on-demand" or
-        (cl == "spot" and not disable_spot and (name, zone) in spot_instances)
-    )
+    return cl in capacity_type and (name, zone) in spot_instances
